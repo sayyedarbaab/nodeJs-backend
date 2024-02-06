63d6ee495c647bced18349aeda7ad6c8dc78e231
Sayyed Arbaab <kazmiwrite@gmail.com>
2024-02-06 23:51:36 +0530

    add commit

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const http = require("http");
 const fs = require("fs");
 const index = fs.readFileSync("index.html", "utf8");
 const data = JSON.parse(fs.readFileSync("db.json")); //JSON.PARSE() json data changing to string
-const product = data.products[7];
+const product = data.products;
 
 const server = http.createServer((req, res) => {
   if (req.url.startsWith("/product")) {
+    const id = req.url.split("/")[2];
+    const prd = product.find((p) => p.id === +id);
+    console.log(prd);
     res.setHeader("Content-type", "text/html");
     const modify = index
       .replace("**title**", product.title)
